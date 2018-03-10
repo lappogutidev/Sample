55b3d023a6d4360268f666e6de15db01e35ecc09
lappogutidev <lappoguti00@gmail.com>
2018-03-10 17:56:54 -0500

    second commit

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // Get data from he attribute
+    func getSample() {
+        let request: NSFetchRequest = SampleEntity.fetchRequest()
+        request.resultType = NSFetchRequestResultType.dictionaryResultType
+        do {
+            let searchResults = try context.fetch(request as! NSFetchRequest<NSFetchRequestResult>) as! [NSDictionary]
+            let searchResultsArray = searchResults.map { $0["sampleAttribute"] as! String}
+            print("searchResultsArray", searchResultsArray)
+        } catch {
+            print("Error with request: \(error)")
+        }
+    }
+    
+    // Save to the attribute
+    func setSample() {
+        let saveSample = SampleEntity(context: context)
+        saveSample.sampleAttribute = "Save a new string."
+        do {
+            try context.save()
+        } catch {
+            print("Error with save: \(error)")
+        }
+    }
+    
+    // clear the attribute
+    func resetSample() {
+        let clearSample: NSFetchRequest = SampleEntity.fetchRequest()
+        let deleteResults = NSBatchDeleteRequest(fetchRequest: clearSample as! NSFetchRequest<NSFetchRequestResult>)
+        do {
+            try context.execute(deleteResults)
+            try context.save()
+        } catch {
+            print("Error with save: \(error)")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getSample()
+        setSample()
+        getSample()
+        resetSample()
+        getSample()
     }
 
     override func didReceiveMemoryWarning() {
